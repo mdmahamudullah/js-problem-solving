c53bf7800de01dd07170715d98c03513d7092f66
mdmahamudullah <mahamudullah7k@gmail.com>
2023-02-05 19:56:02 +0600

    Sum of all numbers of an array

---
+++ add-numbers-of-array.js
@@ -0,0 +1,13 @@
+function sumArray(num) {
+    var sum=0;
+    for (let i=0;i<num.length;i++){
+        sum=sum+num[i];
+        console.log(i,num[i],sum);
+
+    }
+    return sum;
+}
+
+const num = [10,20,30,40];
+const res = sumArray(num);
+console.log(res);
