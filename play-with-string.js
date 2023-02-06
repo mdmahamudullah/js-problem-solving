d9da980e076fb23b05835487184d8b552b6cebf4
mdmahamudullah <mahamudullah7k@gmail.com>
2023-02-06 19:06:02 +0600

    Search includes, indexOf, startswith, endswith

---
+++ play-with-string.js
@@ -0,0 +1,18 @@
+const lyrics='hello vaiu kmn acho tomra';
+// index of
+console.log(' index of :');
+console.log(lyrics.indexOf('vaiu'));
+console.log(lyrics.indexOf('vai'));
+console.log(lyrics.indexOf('x'));
+
+// start with
+console.log('\n start with : ');
+console.log(lyrics.startsWith('h'));
+console.log(lyrics.startsWith('he'));
+console.log(lyrics.startsWith('x'));
+
+// start with
+console.log('\n end with : ');
+console.log(lyrics.endsWith('a'));
+console.log(lyrics.endsWith('ra'));
+console.log(lyrics.endsWith('x'));
