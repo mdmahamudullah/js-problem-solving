b99b582e751349badf9a507d70318bc1c343ea48
mdmahamudullah <mahamudullah7k@gmail.com>
2023-02-06 20:45:20 +0600

    pow,abs

---
+++ math-functions-pow-sqrt-etc.js
@@ -0,0 +1,16 @@
+// pow-power
+const result=Math.pow(2,3);
+console.log(result);
+// abs-absolute value
+const num1=20;
+const num2=25;
+const gap=Math.abs(num1-num2);
+console.log(gap);
+// 
+const num3=4.425;
+const num4=4.625;
+const roundNum1=Math.round(num3);
+const roundNum2=Math.round(num4  );
+
+
+
