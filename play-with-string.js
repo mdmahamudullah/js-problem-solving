de52bab7821117b33072266f05de2dd38f2d91be
mdmahamudullah <mahamudullah7k@gmail.com>
2023-02-06 19:40:46 +0600

    split, slice, substring, includes, indexOf, startswith, endswith

--- play-with-string.js
+++ play-with-string.js
@@ -1,18 +1,41 @@
-const lyrics='hello vaiu kmn acho tomra';
+const lyrics='hello vaiu kmn acho tomra.valo aso monu.';
+const lyrics1='hello vaiu kmn acho tomra.valo aso monu.';
 // index of
-console.log(' index of :');
-console.log(lyrics.indexOf('vaiu'));
-console.log(lyrics.indexOf('vai'));
-console.log(lyrics.indexOf('x'));
+// console.log(' index of :');
+// console.log(lyrics.indexOf('vaiu'));
+// console.log(lyrics.indexOf('vai'));
+// console.log(lyrics.indexOf('x'));
 
 // start with
-console.log('\n start with : ');
-console.log(lyrics.startsWith('h'));
-console.log(lyrics.startsWith('he'));
-console.log(lyrics.startsWith('x'));
+// console.log('\n' +'start with : ');
+// console.log(lyrics.startsWith('h'));
+// console.log(lyrics.startsWith('he'));
+// console.log(lyrics.startsWith('x'));
 
 // start with
-console.log('\n end with : ');
-console.log(lyrics.endsWith('a'));
-console.log(lyrics.endsWith('ra'));
-console.log(lyrics.endsWith('x'));
+// console.log('\n' +'end with : ');
+// console.log(lyrics.endsWith('a'));
+// console.log(lyrics.endsWith('ra'));
+// console.log(lyrics.endsWith('x'));
+
+// split
+// console.log('\n' +'split  : ');
+// console.log('char:',lyrics.split(''));
+// console.log('parts:',lyrics.split(' '));
+// console.log('sentences:',lyrics.split('.'));
+
+// slice
+// console.log('\n' +'slice  : ');
+// console.log(lyrics.slice(5,18));
+
+// substr
+
+// substring
+// console.log('\n' +'substring  : ');
+// console.log(lyrics.substring(5,18));
+// concat
+
+// at
+console.log('\n' +'at  : ');
+console.log(lyrics.at(6));
+console.log(`Using an index of ${-6} the item returned is ${lyrics.at(-5)}`);
