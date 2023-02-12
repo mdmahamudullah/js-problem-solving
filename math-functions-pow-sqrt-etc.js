baa36aafba23de0081b8f97ac1deeea87cded3f2
mdmahamudullah <mahamudullah7k@gmail.com>
2023-02-12 07:14:20 +0600

    Math, abs, pow, round, ceil, floor, and random number

--- math-functions-pow-sqrt-etc.js
+++ math-functions-pow-sqrt-etc.js
@@ -6,11 +6,34 @@ const num1=20;
 const num2=25;
 const gap=Math.abs(num1-num2);
 console.log(gap);
-// 
-const num3=4.425;
-const num4=4.625;
+// round number
+const num3=4.125;
+const num4=4.925;
 const roundNum1=Math.round(num3);
-const roundNum2=Math.round(num4  );
+console.log(roundNum1);
+const roundNum2=Math.round(num4);
+console.log(roundNum2);
+//ceil number
+const num5=4.125;
+const num6=4.925;
+const ceilNum1=Math.ceil(num5);
+console.log(ceilNum1);
+const ceilNum2=Math.ceil(num6);
+console.log(ceilNum2);
+//floor number
+const num7=4.125;
+const num8=4.925;
+const floorNum1=Math.floor(num7);
+console.log(floorNum1);
+const floorNum2=Math.floor(num8);
+console.log(floorNum2);
+//random number
+let num9 = Math.random();
+let num10 = Math.random() * 100;
+let num11 = Math.floor((Math.random() * 100) + 1);
+console.log(num9);
+console.log(num10);
+console.log(num11);
 
 
 
